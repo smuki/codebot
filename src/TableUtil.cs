6032c55ca26c068cdcc502ea7e3064c786d78eb4
yjy <yjy@ekuaibao.com>
2021-06-11 09:57:26 +0800

    Update TableUtil.cs

--- src/TableUtil.cs
+++ src/TableUtil.cs
@@ -1738,7 +1738,7 @@ namespace Volte.Bot.Term
 
             JSONObject _AlterColumns = new JSONObject();
 
-            JSONObject _JSONObject = AppSetting.LoadJSONObject(AppSetting.AppSetting+@"MsSqlToMySqlDataType.json");
+            JSONObject _JSONObject = AppSetting.LoadSetting("MsSqlToMySqlDataType.json");
 
             string sPrev = "";
             foreach (JSONObject colname in nameValues) {
