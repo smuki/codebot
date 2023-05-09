dd013dd9e9d15737053e52c4be111afc9e62fd90
sunriseyuen <tiptoes@foxmail.com>
2023-05-09 21:45:56 +0800

    Create MappingPair.cs

---
+++ src/MappingPair.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Volte.Bot.Term
+{
+    public class MappingPair
+    {
+        public string fName { get; set; }
+        public string tName { get; set; }
+    }
+}
